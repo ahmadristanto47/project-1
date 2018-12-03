7376a1383ca3eb6293e2aaa06a66efc0bdd03751
Ahmad Ristanto <ahmad.ristanto@students.amikom.ac.id>
2018-12-03 16:48:15 +0700

    tambah di Array2.java

--- Array2.java
+++ Array2.java
@@ -3,21 +3,7 @@ public class Array2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] number = {1,25,39,46,52,61,73,82,};
-		
-		for (int i=0; i<number.length; i++){
-			if(number[i]%2 == 0){
-				if(number[i]>20)
-					System.out.println(number[i] +" adalah genap dan lebih dari 20");
-				else System.out.println(number[i]+" adalah genap");
-			}else{
-				if(number[i]>20)
-					System.out.println(number[i] +" adalah ganjil dan lebih dari 20");
-				else System.out.println(number[i]+" adalah ganjil");
-			}
-		}
-		
-		
+		System.out.println("heyy..................");
 	}
 
 }
