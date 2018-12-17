05717b82f83650aebd4d88c05c39425ab4a0c318
ahmadristanto47 <ahmad,ristanto@students.amikom.ac.id>
2018-12-17 16:17:47 +0700

    kuis

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Array2 {
+
+    public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
+        float rupiah, dolar, euro;
+        System.out.print("Masukan Rupiah = ");
+        rupiah = masukan.nextFloat();
+        dolar = (float) (rupiah*0.0001);
+        euro = (float) (rupiah*0.00004);
+        System.out.println();
+        System.out.println("Hasil Kurs = ");
+        System.out.println("Dolar = " + dolar);
+        System.out.println("Euro = " + euro);
+    }
+}
